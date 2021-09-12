e0c94a288f824de30331fbf6d786d701387552a6
MaxR522 <ranjamario@gmail.com>
2021-09-12 11:33:48 +0300

    add timestamp comment

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-  author: {
-    type: String,
-    required: true,
+const commentSchema = new mongoose.Schema(
+  {
+    author: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    carId: {
+      type: String,
+      required: true,
+    },
   },
-  content: {
-    type: String,
-    required: true,
-  },
-  carId: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true },
+);
 
 const Comment = mongoose.model('Comment', commentSchema);
 
